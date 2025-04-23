a32987b013243c8f43cf940344c646ce10667014
Umair Nadeem <umiiawan06@gmail.com>
2025-04-23 16:00:41 +0500

    HTML Reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,19 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  e2e: {
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
+  
+    e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
     baseUrl: 'https://naveenautomationlabs.com/opencart/index.php?route=account/login',
     env: {
       registerUrl: 'https://naveenautomationlabs.com/opencart/index.php?route=account/register'
